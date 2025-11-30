651bef0e37b534afb95d271f7a257473eee1bbb6
akuriprabh <36980256+akuriprabh@users.noreply.github.com>
2025-11-30 16:44:47 -0600

    Update settings.py

--- SevenElements/settings.py
+++ SevenElements/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$9fit3(xv=-l66(hsjd-ouec8i78_!$5&+2bjj7uwk%9+7l&0#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -132,5 +132,5 @@ LOGOUT_REDIRECT_URL = '/login/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-load_dotenv()
+
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
